6369fac1abfe65591353c3de6747af1d3cbefbf7
noah tenser <ntenser@hotmail.com>
2017-02-01 22:15:53 -0500

    first commit

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -5,30 +5,33 @@
 
 
 # 1a) Find the genre with the name "Hip Hop/Rap".
-
+Genre.where("name = 'Hip Hop/Rap'")
 
 
 # 1b) Count how many tracks belong to the "Hip Hop/Rap" genre
-
+Genre.where("name = 'Hip Hop/Rap'")
+Track.where(album_id: 17)
 
 
 # 2) Find the total amount of time required to listen to all the tracks in the database.
 
-
+Track.sum(:milliseconds)
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
-
-
+MediaType.find_by(name: 'MPEG audio file').id
+Track.where(media_type_id: 1).maximum(:unit_price)
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
-
+Track.find_by(media_type_id: 1, unit_price: '0.99').name
 
 
 # 4) Find the 2 oldest artists.
 
-
+Artist.order(:created_at)
+Artist.order(:created_at).limit(2)
 
 ### Stretch Exercise (Active Record Query Interface)
 
 
 # 1) Find all the albums whose titles start with B.
+Album.where("name = ?", 'B')
--- sql_assignment.sql
+++ sql_assignment.sql
@@ -5,19 +5,24 @@
 -- below each of the following questions:
 
 -- 1) Find the albums recorded by the artist Queen.
-
+SELECT id FROM artists WHERE name='Queen';
+SELECT * FROM albums WHERE artist_id=51;
 
 
 -- 2) [Count](http://www.w3schools.com/sql/sql_func_count.asp) how many tracks belong to the media type "Protected MPEG-4 video file".
-
+SELECT id, name FROM media_types;
+SELECT COUNT (*) FROM tracks WHERE tracks.media_type_id=3;
+= 214
 
 
 -- 3) Find the least expensive track that has the genre "Electronica/Dance".
-
-
+SELECT id, name FROM genres; --> 15
+SELECT id, name, unit_price FROM tracks WHERE unit_price <= 0.99 and genre_id=15;
 
 -- 4) Find the all the artists whose names start with A.
 
-
+SELECT * FROM artists WHERE name LIKE 'A%';
 
 -- 5) Find all the tracks that belong to playlist 1.
+SELECT id, name FROM playlists;
+SELECT * FROM playlists_tracks where playlist_id=1;
